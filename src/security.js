ff31f0d2e5360c899f489ab65fba127a76729a9c
comunicacion-wq <comunicacion@itcc.edu.mx>
2026-04-03 21:18:04 -0500

    Update security.js

--- src/security.js
+++ src/security.js
@@ -1,5 +1,7 @@
 export function requireAuth(req, res, next) {
-  if (!req.session.user) return res.redirect("/login");
+  if (!req.session.user) {
+    return res.redirect("/login?next=" + encodeURIComponent(req.originalUrl));
+  }
   next();
 }
 
