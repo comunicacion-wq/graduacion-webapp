78281dfe48dbce321563420d039dea202da4d7f2
comunicacion-wq <comunicacion@itcc.edu.mx>
2026-07-14 22:45:54 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -50,6 +50,11 @@ app.set("views", path.join(__dirname, "views"));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  express.static(
+    path.join(__dirname, "public")
+  )
+);
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(session({
   secret: process.env.SESSION_SECRET || "dev_secret",
